a448f4f47f611c01c52c06072cc6a6a6ec0090e2
Edinyanga O. Ottoho <elzucky@gmail.com>
2019-08-06 21:07:21 +0100

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,14 +1,16 @@
+/* The err() function is a function meant to toggle a div that stores the message. The parameters parsed are message
+i.e err(message) and it pops out, displaying the given message */
 var id = 0;
-var uscanada = ['Next Day Delivery ($20)','3-4 Days ($10)','7 Days ($5)'];
-var europe = ['By air (7 days, $25)','By sea (28 days, $10)'];
-var other = ['By air (10 days, $35)','By sea (28 days, $30)'];
-var shippingid = '1';
-var nation = '';
+var uscanada = ['Next Day Delivery ($20)','3-4 Days ($10)','7 Days ($5)']; //An array to hold dropdown for US/Canada Shipping types
+var europe = ['By air (7 days, $25)','By sea (28 days, $10)']; //An array to hold dropdown for Europe Shipping types
+var other = ['By air (10 days, $35)','By sea (28 days, $30)']; //An array to hold dropdown for Rest of World Shipping types
+var shippingid = '1'; //Default instance of shipping ID
+var nation = ''; //Nation (Empty)
 function pop(x) {
-	_("full-screen-size").style.display = 'block';
+	_("full-screen-size").style.display = 'block'; //Toggles the update form
 	id = x;
 }
-function err(message) {
+function err(message) { //Message popup
     document.querySelectorAll("#error")[0].style.display = 'block';
     document.querySelectorAll("#error")[0].innerHTML = '<center>'+message+'</center>';
     document.querySelectorAll("#error")[0].style.transition = '0.6s';
@@ -20,7 +22,7 @@ function err(message) {
         fin();
     },2500);
 }
-function fin() {
+function fin() {//Hides the message popup after the message is served
     document.querySelectorAll("#error")[0].style.transition = '0.6s';
     document.querySelectorAll("#error")[0].style.opacity = '0';
     document.querySelectorAll("#error")[0].style.top = '-200px';
@@ -29,6 +31,8 @@ function fin() {
         document.querySelectorAll("#error")[0].style.display = 'none';
     },500);
 }
+/* This block adds the appropriate data required when the select option value is changed to match the corresponding shipping details
+Like US/Canada fetches the array us/canada and so on */
 function inptype(x,y) {
 	if (x == 'region') {
 		if (y == 'US/Canada') {
@@ -54,7 +58,7 @@ function inptype(x,y) {
 		}
 	}
 }
-_("uporders").onclick = function(){
+_("uporders").onclick = function(){ //Serves the purpose of updating the order shipping changes
 	nation = _("nation").value;
 	type = _("type").value;
 	var xhttp;
