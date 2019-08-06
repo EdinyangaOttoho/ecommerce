bccb08fdaee8c673ab5a2ad9f86d9507802cb7e9
Edinyanga O. Ottoho <elzucky@gmail.com>
2019-08-06 21:36:11 +0100

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,5 +1,5 @@
 <?php
-    header('Access-Control-Allow-Origin:https://www.247naijaforum.com');
+    header('Access-Control-Allow-Origin:*');
     header('Access-Control-Allow-Credentials:true');
 ?>
 <?php
